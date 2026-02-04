565e90381d5fde3f5a62a8611d875aefcd6aab0e
atif721 <abdullahatif721@outlook.com>
2026-02-04 11:57:07 +0600

    Auth Layout config started

---
+++ frontend/app/routes/Root/HomePage.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarBadge, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import type { Route } from "./+types/home";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }];
+}
+
+const HomePage = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center gap-4">
+      <Link to="/sign-in">
+        <Button className="bg-blue-500 text-white">Login</Button>
+      </Link>
+      <Link to="/sign-up">
+        <Button variant="outline" className="bg-blue-500 text-white">
+          Sign Up
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default HomePage;
