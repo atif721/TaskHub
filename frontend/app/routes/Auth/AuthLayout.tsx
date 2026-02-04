34b7b7f5bf16d59520155deffb881c00f73d7d3a
atif721 <abdullahatif721@outlook.com>
2026-02-04 12:14:12 +0600

    ShadCn New Components Added Form Input Label

---
+++ frontend/app/routes/Auth/AuthLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router";
+const AuthLayout = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <Outlet />
+    </div>
+  );
+};
+
+export default AuthLayout;
