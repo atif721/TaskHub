a543fe12373cb7e8123001893e9b2bd21a0a5124
atif721 <abdullahatif721@outlook.com>
2026-02-04 12:09:31 +0600

    Routes Added

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,12 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/Root/HomePage.tsx")] satisfies RouteConfig;
+export default [
+  layout("routes/Auth/AuthLayout.tsx", [
+    index("routes/Root/HomePage.tsx"),
+    route("sign-in", "routes/Auth/SignIn.tsx"),
+    route("sign-up", "routes/Auth/SignUp.tsx"),
+    route("forgot-password", "routes/Auth/ForgotPassword.tsx"),
+    route("reset-password", "routes/Auth/ResetPassword.tsx"),
+    route("verify-email", "routes/Auth/VerifyEmail.tsx"),
+  ]),
+] satisfies RouteConfig;
